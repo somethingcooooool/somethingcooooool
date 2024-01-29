f7fa027be91c0cb0fb6be7f9007fde2f98a3d95b
somethingcooooooool <157722464+somethingcooooool@users.noreply.github.com>
2024-01-29 10:14:43 -0500

    madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,13 @@
 answer = ""
 words = []
 
+answer = input("Noun")
+words.append(answer)
+answer = input("Adverb")
+words.append(answer)
+answer = input("Adjective")
+words.append(answer)
+
+i = 0
+for x in range(len(words)
+  print({words[i]})
